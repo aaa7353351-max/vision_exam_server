3259be310b066d0b809ba6ad09d487303189018d
aaa7353351-max <aaa7353351@gmail.com>
2026-02-09 12:02:07 +0900

    MongoDB연결+DB연결코드 config.py

---
+++ app/core/config.py
@@ -0,0 +1,8 @@
+# app/core/config.py
+
+MONGO_URI = "mongodb://localhost:27017"
+
+DB_NAME = "vision_exam"
+COLLECTION_NAME = "inference_results"
+
+UPLOAD_DIR = "uploads"
---
+++ app/infra/db.py
@@ -0,0 +1,8 @@
+# app/infra/db.py
+
+from pymongo import MongoClient
+from app.core.config import MONGO_URI, DB_NAME
+
+client = MongoClient(MONGO_URI)
+
+db = client[DB_NAME]
